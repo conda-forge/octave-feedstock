788d6d0c0b23421cce95f9395b20d9bb39302ea8
conda-forge-admin <pelson.pub+conda-forge@gmail.com>
2019-02-14 10:37:02 +0000

    MNT: Re-rendered with conda-build 3.17.8, conda-smithy 3.2.13, and conda-forge-pinning 2019.02.11

--- .circleci/build_steps.sh
+++ .circleci/build_steps.sh
@@ -31,7 +31,7 @@ source run_conda_forge_build_setup
 # "recipe/yum_requirements.txt" file. After updating that file,
 # run "conda smithy rerender" and this line will be updated
 # automatically.
-/usr/bin/sudo -n yum install -y mesa-libGL mesa-dri-drivers libselinux libXdamage libXfixes libXxf86vm libxcb libXft expat xorg-x11-proto
+/usr/bin/sudo -n yum install -y mesa-libGL mesa-dri-drivers libselinux libXdamage libXfixes libXxf86vm libxcb libXft expat xorg-x11-proto GraphicsMagick-devel GraphicsMagick-c++-devel
 
 
 # make the build number clobber
