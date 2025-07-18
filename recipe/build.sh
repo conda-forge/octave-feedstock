3974744e75f284c69c99d64934f70f8486f2c6f0
Isabel Paredes <isabel.paredes@quantstack.net>
2025-07-18 17:06:37 +0200

    Try restoration

--- recipe/build.sh
+++ recipe/build.sh
@@ -12,19 +12,23 @@ if [[ $target_platform == "linux-ppc64le" || $target_platform == "osx-arm64" || 
     export ax_blas_integer_size=4
 fi
 
-if [[ $target_platform == "osx-arm64" ]]; then
-    echo "OOO osx-arm64 detected"
-    export PKG_CONFIG="${BUILD_PREFIX}/bin/pkg-config"
-    export PKG_CONFIG_PATH="${PREFIX}/lib/pkgconfig:${PREFIX}/share/pkgconfig"
-    $PKG_CONFIG --version
-fi
+# if [[ $target_platform == "osx-arm64" ]]; then
+#     echo "OOO osx-arm64 detected"
+#     export PKG_CONFIG="${BUILD_PREFIX}/bin/pkg-config"
+#     export PKG_CONFIG_PATH="${PREFIX}/lib/pkgconfig:${PREFIX}/share/pkgconfig"
+#     $PKG_CONFIG --version
+# fi
 
-if [[ $target_platform == "osx-64" ]]; then
-    echo "OOO osx-64 detected"
-    export PKG_CONFIG="${PREFIX}/bin/pkg-config"
-    export PKG_CONFIG_PATH="${PREFIX}/lib/pkgconfig:${PREFIX}/share/pkgconfig"
-    $PKG_CONFIG --version
-fi
+# if [[ $target_platform == "osx-64" ]]; then
+#     echo "OOO osx-64 detected"
+#     export PKG_CONFIG="${PREFIX}/bin/pkg-config"
+#     export PKG_CONFIG_PATH="${PREFIX}/lib/pkgconfig:${PREFIX}/share/pkgconfig"
+#     $PKG_CONFIG --version
+# fi
+
+printenv | grep "PKG"
+which pkg-config
+pkg-config --version || true
 
 export octave_cv_working_blas_single_precision=yes
 
