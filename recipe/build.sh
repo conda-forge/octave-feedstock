0fec229509154923cb501c35de34b3ae1c8c7a69
Isabel Paredes <isabel.paredes@quantstack.net>
2025-07-18 13:10:50 +0200

    Fix syntax

--- recipe/build.sh
+++ recipe/build.sh
@@ -16,7 +16,8 @@ if [[ $target_platform == "osx-arm64" ]]; then
     echo "OOO osx-arm64 detected"
     export PKG_CONFIG="${BUILD_PREFIX}/bin/pkg-config"
     export PKG_CONFIG_PATH="${PREFIX}/lib/pkgconfig:${PREFIX}/share/pkgconfig"
-else if [[ $target_platform == "osx-64" ]]; then
+fi
+if [[ $target_platform == "osx-64" ]]; then
     echo "OOO osx-64 detected"
     export PKG_CONFIG="${PREFIX}/bin/pkg-config"
     export PKG_CONFIG_PATH="${PREFIX}/lib/pkgconfig:${PREFIX}/share/pkgconfig"
