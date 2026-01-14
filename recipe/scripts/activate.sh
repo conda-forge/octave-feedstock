62e32628013b40ccd2f088cdc445123f05586163
arjxn-py <arjxn.py@gmail.com>
2026-01-14 14:51:13 +0530

    Rebuild Octave global package registry on environment activation

--- recipe/scripts/activate.sh
+++ recipe/scripts/activate.sh
@@ -1,2 +1,5 @@
 export OCTAVE_HOME_CONDA_BACKUP=${OCTAVE_HOME:-}
 export OCTAVE_HOME=$CONDA_PREFIX
+
+# Rebuild Octave global package registry so globally installed
+octave --eval "pkg rebuild -global"
