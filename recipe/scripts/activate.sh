197f6ccb8d3ed64f6cb788f5bcb4e9202eb7e730
arjxn-py <arjxn.py@gmail.com>
2026-01-16 17:30:18 +0530

    complete comment

--- recipe/scripts/activate.sh
+++ recipe/scripts/activate.sh
@@ -1,5 +1,5 @@
 export OCTAVE_HOME_CONDA_BACKUP=${OCTAVE_HOME:-}
 export OCTAVE_HOME=$CONDA_PREFIX
 
-# Rebuild Octave global package registry so globally installed
+# Rebuild Octave global package registry so globally installed Octave Forge packages are discoverable in fresh environments
 octave --eval "pkg rebuild -global"
